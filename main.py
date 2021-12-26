4e9929a3a8af593f29014df8444f9748177a89f2
jade thomas <jade729@gmail.com>
2021-12-26 11:07:33 -0500

    added: clock app + assets

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/clock')
+def clock():
+  return render_template('clock.html')
   
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -52,3 +52,19 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock_face').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
